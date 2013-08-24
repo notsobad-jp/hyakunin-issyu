12a01ef7e10ffc639e0a6fb45bcb7f338c9522ea
tomomichi <tomomichi.onishi@gmail.com>
2013-08-24 15:25:39 +0900

    use module

--- lib/HyakuninIssyu.rb
+++ lib/HyakuninIssyu.rb
@@ -2,11 +2,10 @@ require "HyakuninIssyu/version"
 require "yaml"
 
 module HyakuninIssyu
-	def initialize
-		@hoge = "hoge"
-	end
+	TITLE = "100 poems by 100 poets"
 
 	def calc
 		"hogehoge"
 	end
+	module_function :calc
 end

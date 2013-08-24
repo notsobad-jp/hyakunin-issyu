78abc855600c07203ce4f45fcd313a160f307ab5
tomomichi <tomomichi.onishi@gmail.com>
2013-08-24 15:29:32 +0900

    load yaml file

--- lib/HyakuninIssyu.rb
+++ lib/HyakuninIssyu.rb
@@ -2,7 +2,8 @@ require "HyakuninIssyu/version"
 require "yaml"
 
 module HyakuninIssyu
-	TITLE = "100 poems by 100 poets"
+  AUTHORS = YAML.load_file(File.expand_path(File.join('..', 'data', 'authors.yml'), __FILE__))
+  POEMS = YAML.load_file(File.expand_path(File.join('..', 'data', 'poems.yml'), __FILE__))
 
 	def calc
 		"hogehoge"

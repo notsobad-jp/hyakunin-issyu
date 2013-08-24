d731757190eafd48bf0468726658c852a62acd3f
tomomichi <tomomichi.onishi@gmail.com>
2013-08-25 08:38:28 +0900

    enable kana output

--- lib/HyakuninIssyu.rb
+++ lib/HyakuninIssyu.rb
@@ -19,32 +19,90 @@ class HyakuninIssyu
 		def initialize(id)
 			poems = YAML.load_file(File.expand_path(File.join('..', 'data', 'poems.yml'), __FILE__))
 			@poem = poems[id-1]
+			@full = Full.new(@poem)
+			@first = First.new(@poem)
+			@last = Last.new(@poem)
 		end
 
-		def full
+		def kana
+			@poem["kana"]
+		end
+
+		def kanji
 			@poem["poem"]
 		end
 
+		def full
+			@full
+		end
+
 		def first
-			poem_data = @poem["poem"].split(" ")
-			first = ''
-			for i in 0..2
-				first << poem_data[i]
-			end
-			return first
+			@first
 		end
 
 		def last
-			poem_data = @poem["poem"].split(" ")
-			first = ''
-			for i in 3..4
-				first << poem_data[i]
-			end
-			return first
+			@last
 		end
 
 		def comment
 			@poem['comment']
+		end
+
+		class Full
+			def initialize(data)
+				@full_kanji = data["poem"]
+				@full_kana  = data["kana"]
+			end
+
+			def kanji
+				@full_kanji
+			end
+
+			def kana
+				@full_kana
+			end
+		end
+
+		class First
+			def initialize(data)
+				kanji_data = data["poem"].split(" ")
+				@first_kanji = ''
+				for i in 0..2
+					@first_kanji << kanji_data[i]
+				end
+
+				kana_data = data["kana"].split("　")
+				@first_kana = kana_data[0]
+			end
+
+			def kana
+				@first_kana
+			end
+
+			def kanji
+				@first_kanji
+			end
+		end
+
+		class Last
+			def initialize(data)
+				kanji_data = data["poem"].split(" ")
+				@last_kanji = ''
+				for i in 3..4
+					@last_kanji << kanji_data[i]
+				end
+
+				kana_data = data["kana"].split("　")
+				@last_kana = kana_data[1]
+			end
+
+			def kana
+				@last_kana
+			end
+
+			def kanji
+				@last_kanji
+			end
 		end
 	end
 

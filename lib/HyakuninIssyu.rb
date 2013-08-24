3b0ad3178f87664776362d49319b803572c9d8ce
tomomichi <tomomichi.onishi@gmail.com>
2013-08-24 15:58:13 +0900

    use inner class

--- lib/HyakuninIssyu.rb
+++ lib/HyakuninIssyu.rb
@@ -2,10 +2,35 @@ require "HyakuninIssyu/version"
 require "yaml"
 
 module HyakuninIssyu
-  AUTHORS = YAML.load_file(File.expand_path(File.join('..', 'data', 'authors.yml'), __FILE__))
-  POEMS = YAML.load_file(File.expand_path(File.join('..', 'data', 'poems.yml'), __FILE__))
 
-	def poem(id)
+	class Poem
+		def initialize(id)
+			poems = YAML.load_file(File.expand_path(File.join('..', 'data', 'poems.yml'), __FILE__))
+			POEM = poems[id-1]
+		end
+
+		def full(poem)
+			poem.poem
+		end
+
+		def first(poem)
+			poem.poem
+		end
+
+		def last(poem)
+			poem.poem
+		end
+
+		def comment(poem)
+			poem.comment
+		end
+
+		def name
+		end
 	end
 	module_function :poem
+
+	class Poet
+		POETS = YAML.load_file(File.expand_path(File.join('..', 'data', 'poets.yml'), __FILE__))
+	end
 end

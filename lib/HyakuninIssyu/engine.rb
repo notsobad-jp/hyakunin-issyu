dbd77d7e3620667f5179c04c8a28848b7f2e2124
tomomichi <tomomichi.onishi@gmail.com>
2015-12-22 15:29:16 +0900

    add assets path

--- lib/HyakuninIssyu/engine.rb
+++ lib/HyakuninIssyu/engine.rb
@@ -1,5 +1,10 @@
 class HyakuninIssyu
   class Engine < ::Rails::Engine
     isolate_namespace HyakuninIssyu
+
+    initializer :assets do |config|
+      Rails.application.config.assets.paths << root.join("vendor", "assets", "images")
+      Rails.application.config.assets.paths << root.join("vendor", "assets", "audios")
+    end
   end
 end
